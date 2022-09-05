e50dceaeed50665f653a9daeda362cc81d5429bc
clwmsms12 <clwmsms12@naver.com>
2022-09-05 14:06:28 +0900

    full0905

--- 20220905_fullpage/fullpage/js/script.js
+++ 20220905_fullpage/fullpage/js/script.js
@@ -1,1 +1,8 @@
-$("#fullpage").fullpage()
+$("#fullpage").fullpage({
+    menu:".navTop",
+    anchors:["m1st","m2st","m3st","m4st"],
+    navigation:true,
+    navigationPosition:"fp-right",
+    navigationTooltips:["furst","second","third","forth"],
+    responsiveWidth:900     //크기가 줄어졌을때 스크롤이 생김
+})
