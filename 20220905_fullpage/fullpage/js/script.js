5aec7a189ec00b2d3f6af660e5c88ecbf9f07d71
clwmsms12 <clwmsms12@naver.com>
2022-09-06 14:42:55 +0900

    fullpageTextMove

--- 20220905_fullpage/fullpage/js/script.js
+++ 20220905_fullpage/fullpage/js/script.js
@@ -4,5 +4,30 @@ $("#fullpage").fullpage({
     navigation:true,
     navigationPosition:"fp-right",
     navigationTooltips:["furst","second","third","forth"],
-    responsiveWidth:900     //크기가 줄어졌을때 스크롤이 생김
+    responsiveWidth:900,     //크기가 줄어졌을때 스크롤이 생김
+    afterLoad:function(anchorslink,index){
+        if(index == 1){
+            $(".page1 .txtWrap").addClass("move")
+        }else{
+            $(".page1 .txtWrap").removeClass("move")
+        }
+
+        if(index == 2){
+            $(".page2 .txtWrap").addClass("move")
+        }else{
+            $(".page2 .txtWrap").removeClass("move")
+        }
+
+        if(index == 3){
+            $(".page3 .wrap").addClass("move")
+        }else{
+            $(".page3 .wrap").removeClass("move")
+        }
+
+        if(index == 4){
+            $(".page4 .txtWrap").addClass("move")
+        }else{
+            $(".page4 .txtWrap").removeClass("move")
+        }
+    }
 })
---
+++ 20220905_fullpage/fullpage/js/script01.js
@@ -0,0 +1,8 @@
+$(".visual").fullpage({
+    menu:".nav",
+    anchors:["m1st","m2st","m3st","m4st"],
+    navigation:true,
+    navigationPosition:"fp-right",
+    navigationTooltips:["furst","second","third","forth"],
+    responsiveWidth:900
+})
