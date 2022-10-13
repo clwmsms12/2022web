10748098cbac50c49f935613d7063403460328e0
clwmsms12 <clwmsms12@naver.com>
2022-10-13 16:13:44 +0900

    vue modal

---
+++ vueproject/new-20221013/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <web-banner :banData="bData" @modalOpen="modalView"></web-banner>
+
+  <div class="start" :class="{end:mView}">
+
+    <web-modal :mView="mView" :mData="mData" :banData="bData" @closeModal="mView=false"></web-modal>
+
+  </div>
+</template>
+
+<script>
+// import "./assets/style.css"
+import bData from "@/data/directs.js";
+import WebBanner from "@/components/WebBanner.vue";
+import WebModal from "@/components/WebModal.vue";
+export default {
+  components: {
+    WebBanner,
+    WebModal,
+  },
+  data() {
+    return {
+      bData: bData,
+      mView:false,
+      mData:1,
+    };
+  },
+  methods:{
+    modalView(num,name){
+      console.log(num,name);
+      this.mView = true;
+      this.mData = num;
+    }
+  }
+};
+</script>
+
+<style>
+@import url(./assets/style.css);
+.start{
+  opacity: 0;
+  transition: 0.5s;
+}
+.start.end{
+  opacity: 1;
+}
+</style>
---
+++ vueproject/new-20221013/src/components/WebModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="modal" :class="{'d-block':mView}">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{banData[mData].name}}</h5>
+            <button type="button" class="btn-close" @click="$emit('closeModal')"></button>
+          </div>
+          <div class="modal-body">
+            <img :src="`./images/directs/${banData[mData].src}`" alt="">
+            <p>Modal body text goes here.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["mData","mView","banData"]
+}
+</script>
+
+<style>
+
+</style>
